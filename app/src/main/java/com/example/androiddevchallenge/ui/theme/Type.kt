b0c9ca2fafaf8adb6032c225f29902a71feeebcc
Pushkar Aagnihotri <agnihotripush@gmail.com>
2021-09-04 02:11:20 +0530

    :tada: inital setup and ktlint added

--- app/src/main/java/com/example/androiddevchallenge/ui/theme/Type.kt
+++ app/src/main/java/com/example/androiddevchallenge/ui/theme/Type.kt
@@ -1,43 +1,47 @@
-/*
- * Copyright 2021 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package com.example.androiddevchallenge.ui.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.androiddevchallenge.R
+
+private val KulimParkLight = FontFamily(Font(R.font.kulim_park_light))
+private val KulimParkRegular = FontFamily(Font(R.font.kulim_park_regular))
+private val LatoBold = FontFamily(Font(R.font.lato_bold))
+private val LatoRegular = FontFamily(Font(R.font.lato_regular))
 
 // Set of Material typography styles to start with
-val typography = Typography(
+val Typography = Typography(
+    h1 = TextStyle(
+        fontSize = 28.sp,
+        letterSpacing = 1.15.sp,
+        fontFamily = KulimParkLight,
+    ),
+    h2 = TextStyle(
+        fontSize = 15.sp,
+        letterSpacing = 1.15.sp,
+        fontFamily = KulimParkRegular,
+    ),
+    h3 = TextStyle(
+        fontSize = 14.sp,
+        letterSpacing = 0.sp,
+        fontFamily = LatoBold,
+    ),
     body1 = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp
-    )
-        /* Other default text styles to override
+        fontSize = 14.sp,
+        letterSpacing = 0.sp,
+        fontFamily = LatoRegular,
+    ),
     button = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.W500,
-        fontSize = 14.sp
+        fontSize = 14.sp,
+        letterSpacing = 1.15.sp,
+        fontFamily = LatoBold,
     ),
     caption = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 12.sp
-    )
-    */
+        fontSize = 12.sp,
+        letterSpacing = 1.15.sp,
+        fontFamily = KulimParkRegular,
+    ),
 )
