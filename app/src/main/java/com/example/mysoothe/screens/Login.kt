2499426445916cc3b4487d0ca95a43028d02f262
Pushkar Aagnihotri <agnihotripush@gmail.com>
2022-09-25 14:33:16 +0530

    version up

--- app/src/main/java/com/example/mysoothe/screens/Login.kt
+++ app/src/main/java/com/example/mysoothe/screens/Login.kt
@@ -18,7 +18,6 @@ import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
-import androidx.navigation.compose.navigate
 import androidx.navigation.compose.rememberNavController
 import com.example.mysoothe.R
 import com.example.mysoothe.ui.theme.MyTheme
